59f502b0a15d3963956713144dea13e25dcd0a05
juanma <jpinero@accedia.es>
2026-06-16 12:49:24 +0200

    Funciona Dockerfile pero no compose

---
+++ monotareas/app/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app=express();
+
+const PORT=3000;
+
+app.get('/', (req,res) => {
+	res.send("HOLA!!");
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`Servidor express arrancado en el puerto ${PORT}.`);
+});
+
