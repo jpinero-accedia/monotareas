b660b605434060a92cba5b8715e19f4504b6032e
juanma <jpinero@accedia.es>
2026-06-18 08:46:35 +0200

    acp change

--- monotareas/docker/app/src/db.js
+++ monotareas/docker/app/src/db.js
@@ -1,74 +1,161 @@
-import mariadb     from 'mariadb';
-import initialData from './initial-data.json' with { type: 'json'};
+// vim: ft=javascript: ts=3: sw=3: noet:
 
-const POOL = mariadb.createPool({
-    host:            process.env.DB_HOST     || 'localhost',
-    user:            process.env.DB_USER     || 'root',
-    password:        process.env.DB_PASSWORD || 'secret',
-    database:        process.env.DB_NAME     || 'tareasdb',
-    connectionLimit: 5
+import mariadb from 'mariadb';
+
+import initialData from './initial-data.json' with { type: 'json' };
+
+
+export const fxs = {};
+
+
+class Driver {
+	constructor (indata) {
+		const defdata = {
+			host:            'localhost',
+			user:            'root',
+			password:        undefined,
+			database:        undefined,
+			connectionLimit: 5
+		};
+
+		this.config = Object.fromEntries(
+			Object.entries({ ...defdata, ...indata })
+				.filter( ( [k] ) => k in defdata )
+		);
+	}
+
+
+	#getPool () {
+		return this.pool ?? (
+			this.pool = mariadb.createPool(this.config)
+		);
+	}
+
+
+	async #getConnection () {
+		return await this.#getPool().getConnection();
+	}
+
+
+	async #conn ( handler ) {
+		let conn;
+		let ret = undefined;
+		
+		try {
+			ret = await handler(conn);
+		}
+		catch (err) {
+			console.error(`La operación con la conexión falló: ${err}`);
+		}
+		finally {
+			if (conn) {
+				conn.end();
+			}
+		}
+
+		return ret;
+	}
+
+
+	async query (sql) {
+		return await this.#conn(
+			async conn => await conn.query(sql)
+		);
+	}
+
+	async execute (sql, params=[]) {
+		return await this.#conn(
+			async conn => await conn.execute(sql, params)
+		);
+	}
+
+
+	async #createSchema () {
+		await this.query(`
+			CREATE TABLE tareas (
+				id     INT          AUTO_INCREMENT PRIMARY KEY,
+				titulo VARCHAR(255) NOT NULL
+			);
+		`);
+	}
+
+
+	async initialiseData () {
+		await this.#conn(
+			async conn => {
+				await initialData
+					.map( e => e.titulo )
+					.forEach( async titulo => await fxs.dbInsertData(titulo) )
+			}
+		);
+	}
+
+
+	async #doCheckSchema () {
+		const tablas = await this.query("SHOW TABLES LIKE 'tareas';");
+		return ( tablas.length !== 0 );
+	}
+
+
+	async checkSchema () {
+		if ( ! await this.#doCheckSchema() ) {
+			console.log("No existe el esquema de la base de datos.");
+
+			// Lo creamos
+			await this.#createSchema();
+
+			// Añadimos los datos de inicio
+			await this.initialiseData();
+		}
+	}
+
+
+	async deleteAll () {
+		return await this.execute('DELETE FROM tareas;');
+	}
+}
+
+
+// Creamos un driver global
+const theDriver = new Driver({
+	host:     process.env.DB_HOST     || 'localhost',
+	user:     process.env.DB_USER     || 'root',
+	password: process.env.DB_PASSWORD || 'secret',
+	database: process.env.DB_NAME     || 'tareasdb',
+	pp: 34
 });
 
-async function getConnection () {
-    return await POOL.getConnection();
+
+// Funciones que el modulo exporta
+fxs.dbGetAll = async function () {
+	return await this.query('SELECT id, titulo FROM tareas;');
 }
 
-async function initData () {
-    let conn;
 
-    console.log("====> Cargando datos iniciales.");
-
-    try {
-        conn = await getConnection();
-
-        const xformData = initialData
-            .map( x => x.titulo )
-            .forEach( async titulo => {
-                await conn.query(`INSERT INTO tareas (titulo) VALUES ("${titulo}")`);
-            });
-    }
-    catch (err) {
-        console.err(`Error al insertar datos de inicio: ${err}`);
-    }
-    finally {
-        if (conn) {
-            conn.end();
-        }
-    }
+fxs.dbGetOne = async function (id) {
+	return await this.query(`SELECT id, titulo FROM tareas WHERE id=${id};`);
 }
 
-export async function initDb () {
-    let conn;
 
-    console.log('===> Comprobando si el esquema de la DB existe.');
-
-    try {
-                conn = await getConnection();
-        const tablas = await conn.query("SHOW TABLES LIKE 'tareas'");
-
-        if (tablas.length === 0) {
-            console.log('No existe el esquema de la DB!!! --> A CREARLO');
-
-            await conn.query(`
-                CREATE TABLE tareas (
-                    id     INT          AUTO_INCREMENT PRIMARY KEY,
-                    titulo VARCHAR(255) NOT NULL
-                )    
-            `);
-
-            await initData();
-        }
-        else {
-            console.log('Esquema de la DB ya estaba creado.');
-        }
-    }
-    catch (err) {
-        console.err(`Error al verificar o crear esquema: ${err}`);
-    }
-    finally {
-        if (conn) {
-            conn.end();
-        }
-    }
+fxs.dbInsertData = async function (titulo) {
+	return await this.execute(`INSERT INTO tareas (titulo) VALUES ("${titulo}")`);
 }
 
+
+fxs.dbReset = async function () {
+	const ret = await this.deleteAll();
+
+	await this.initialiseData();
+
+	return ret;
+}
+
+
+// con el driver global, comprobamos si hay esquema o tenemos que recrearlo
+(function () {
+	await theDriver.checkSchema();
+}) ();
+
+
+// Exportamos nuestro objeto por defecto
+export default fxs;
--- monotareas/docker/app/src/server.js
+++ monotareas/docker/app/src/server.js
@@ -1,14 +1,25 @@
-import express from 'express';
+// vim: ft=javascript: ts=3: sw=3: noet:
 
-import { initDb } from './db.js';
+import express from 'express';
+import DB      from './db.js';
 
 const app=express();
-
 const PORT=3000;
 
-app.get('/', (req,res) => {
-	res.send("HOLA!!");
-});
+
+// middleware de JSON
+app.use(express.json());
+
+
+// RUTAS
+// app.get('/', (req,res) => {
+// 	res.send("HOLA!!");
+// });
+
+app.get('/tareas', async (req,res) => {
+	res.json(await DB.getAll());
+} );
+
 
 app.listen(PORT, '0.0.0.0', () => {
 	console.log(`Servidor express arrancado en el puerto ${PORT}.`);
